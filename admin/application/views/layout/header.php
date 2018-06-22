5acd91e9458a1adf16c59966ee01022364745365
anangnovriadi <anangnov99@gmail.com>
2018-06-22 20:29:19 +0700

    add assets

---
+++ admin/application/views/layout/header.php
@@ -0,0 +1,19 @@
+<header class="main-header dark-bg"> 
+    <a href="./index.html" class="logo dark-bg"> 
+    <span class="logo-mini"><img src="<?php echo base_url('assets/admin/img/logo.png'); ?>" alt="Ovio" /></span> 
+    <span class="logo-lg"><img src="<?php echo base_url('assets/admin/img/logo-admin.png'); ?>" alt="Ovio" /></span> </a> 
+    <nav class="navbar navbar-static-top" role="navigation"> 
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button"> <span class="sr-only">Toggle navigation</span> </a>
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <img src="<?php echo base_url('assets/admin/img/img1.jpg'); ?>" class="user-image" alt="User Image" /> <span class="hidden-xs">Florence Douglas</span> </a>
+                <ul class="dropdown-menu">
+                    <li><a href="#"><i class="icon-gears"></i> Account Setting</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
+                </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
