3cbf2e15c685e8026ccfbc4484516a4551f5fca4
anangnovriadi <anangnov99@gmail.com>
2018-06-24 00:48:23 +0700

    pesawat add, view

--- admin/application/controllers/Pesawat.php
+++ admin/application/controllers/Pesawat.php
@@ -1,20 +1,61 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Pesawat extends CI_Controller {
+class Pesawat extends CI_Controller 
+{
     public function __construct() 
     {
         parent::__construct();
+
+        $this->load->model('mdlpesawat');
     }
 
     public function index() 
     {
+        $getDataP = $this->mdlpesawat->getPesawat('tb_pesawat')->result();
+        $data['tmpPesawat'] = $getDataP;
 
+        $this->load->view('pesawat/dataPesawat', $data);
     }
 
     public function addPesawat() 
     {
         $this->load->view('pesawat/addPesawat');
+    }
+
+    public function createPesawat() 
+    {
+        $nama_pesawat = $this->input->post('nama_pesawat');
+        $keberangkatan = $this->input->post('keberangkatan');
+        $tujuan = $this->input->post('tujuan');
+        $jam_keberangkatan = $this->input->post('jam_keberangkatan');
+        $jam_tiba = $this->input->post('jam_tiba');
+        $kode_pesawat = $this->input->post('kode_pesawat');
+        $kelas_penerbangan = $this->input->post('kelas_penerbangan');
+        $stok = $this->input->post('stok');
+        $harga = $this->input->post('harga');
+        $reg = array(',00', 'Rp', '.');
+        $regStr = array('', '', '');
+
+        $hargaStr = str_replace($reg, $regStr, $harga);
+
+        $dataInsert = array(
+            'id' => '',
+            'nama_pesawat' => $nama_pesawat,
+            'keberangkatan' => $keberangkatan,
+            'tujuan' => $tujuan,
+            'jam_keberangkatan' => $jam_keberangkatan,
+            'jam_tiba' => $jam_tiba,
+            'harga' => $hargaStr,
+            'kode_pesawat' => $kode_pesawat,
+            'kelas_penerbangan' => $kelas_penerbangan,
+            'tersedia' => '1'
+        );
+
+        $this->mdlpesawat->insertPesawat('tb_pesawat', $dataInsert);
+        $this->session->set_flashdata('message', 'Success Add');
+
+        redirect(base_url('admin/pesawat/addPesawat'));
     }
 
 }
---
+++ admin/application/views/pesawat/dataPesawat.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<?php $this->load->view('layout/meta'); ?>
+<?php $this->load->view('layout/css'); ?>
+
+<body class="sidebar-mini">
+<div class="wrapper"> 
+    <?php $this->load->view('layout/header') ?>
+    <?php $this->load->view('layout/sidebar') ?>
+
+    <div class="content-wrapper">
+        <section class="content-header">
+          <h1>Pesawat</h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-home"></i> Pesawat</a></li>
+            <li class="active"><i class="fa fa-dashboard"></i> Data</li>
+          </ol>
+        </section>
+      
+        <section class="content container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="chart-box">
+                        <h4>Data Tiket Pesawat</h4>
+                        <div class="table-responsive m-top-2">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nama Kapal</th>
+                                    <th>Kode Pesawat</th>
+                                    <th>Keberangkatan</th>
+                                    <th>Tujuan</th>
+                                    <th>Jam Berangkat</th>
+                                    <th>Jam Tiba</th>
+                                    <th>Kelas Penerbangan</th>
+                                    <th>Harga</th>
+                                    <th>Kesediaan</th>
+                                    <th>Tools</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                    $no = 1;
+
+                                    foreach($tmpPesawat as $rows) {
+                                ?>
+                                    <tr>
+                                        <td><?php echo $no; ?></td>
+                                        <td><?php echo $rows->nama_pesawat; ?></td>
+                                        <td><?php echo $rows->kode_pesawat; ?></td>
+                                        <td><?php echo $rows->keberangkatan; ?></td>
+                                        <td><?php echo $rows->tujuan; ?></td>
+                                        <td><?php echo $rows->jam_keberangkatan; ?></td>
+                                        <td><?php echo $rows->jam_tiba; ?></td>
+                                        <td><?php echo $rows->kelas_penerbangan; ?></td>
+                                        <td><?php echo $rows->harga; ?></td>
+                                        <td>
+                                            <?php
+                                                if($rows->tersedia == 1) {
+                                                    echo "<span class='lable-tag tag-success'>Tersedia</span>";
+                                                } else if($rows->tersedia == 0) {
+                                                    echo "<span class='lable-tag tag-unpaid'>Habis</span>";
+                                                }
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <center>
+                                                <a href=""><i class="fa fa-edit fa-lg"></i></a>
+                                                <span class="spasi">
+                                                <a href=""></span><i class="fa fa-trash fa-lg"></a></i>
+                                            </center>
+                                        </td>
+                                    </tr>
+                                <?php
+                                    $no++;
+                                    }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        
+    </div> 
+
+    <?php $this->load->view('layout/footer'); ?>
+</div>
+
+<?php $this->load->view('layout/js'); ?>
+
+</body>
+</html>
