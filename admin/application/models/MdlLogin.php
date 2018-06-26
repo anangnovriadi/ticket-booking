6f67687168509e7032f0121584315aba24bc21f4
anangnovriadi <anangnov99@gmail.com>
2018-06-27 04:15:57 +0700

    login controller

--- admin/application/models/MdlLogin.php
+++ admin/application/models/MdlLogin.php
@@ -3,7 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MdlLogin extends CI_Model 
 {
-    
+    public function authAdmin($username, $password)
+    {
+        $query = $this->db->query("SELECT * FROM tb_users WHERE username = '$username' AND password = '$password' AND status = '1'");
+        return $query;
+    }
 }
 
 ?>
