0c85767f7d41d5c78af45cd5f97507f91a2f8f79
anangnov99@gmail.com <anangnov99@gmail.com>
2018-08-18 21:35:19 +0700

    login front

--- application/views/resultSearch.php
+++ application/views/resultSearch.php
@@ -64,8 +64,6 @@
 	
 </div>
 
-<?php $this->load->view('auth/login'); ?>
-
 	<a href="#header" id="backtotop" class="bg-purple"><i class="fa fa-chevron-up fa-2x"></i></a>
 
 <?php $this->load->view('layout/js'); ?>
