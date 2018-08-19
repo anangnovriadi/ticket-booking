d214b685185c38cf73aca0807e85fab182b77467
anangnov99@gmail.com <anangnov99@gmail.com>
2018-08-20 02:55:11 +0700

    layout login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -13,7 +13,21 @@
     <div class="section" style="margin-bottom: 8%;">
         <div class="mt-inner">
             <div class="pt-inner2">
-                <h4>Login</h4>  
+                <div style="margin-top: 6%;">
+                    <div class="modal-content md-cs">
+                        <div class="modal-header bg-purple text-white">
+                            <h3 class="modal-title" id="myModalLabel"><i class="fa fa-user"></i> Login</h3>
+                        </div>
+                        <div class="modal-body">
+                            <input type="text" name="username" class="form-control" placeholder="Username" autofocus=""><br>
+                            <input type="password" name="password" class="form-control" placeholder="Password" autofocus=""><br>
+                            <input type="submit" class="btn btn-block bg-purple" value="LOGIN"><br>
+                            <div class="reg">
+                               <p>Belum punya akun? Register <a href="">disini</a></p>     
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
