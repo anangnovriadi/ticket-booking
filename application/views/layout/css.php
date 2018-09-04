f4e8e38a3346bbb3f5fd369ddc538983b6de4dbd
anangnov99@gmail.com <anangnov99@gmail.com>
2018-09-04 20:41:22 +0700

    style dropdown

--- application/views/layout/css.php
+++ application/views/layout/css.php
@@ -4,3 +4,4 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/front/css/bootstrap.min.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/front/css/font-awesome.min.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/front/css/hover.css'); ?>">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -5,23 +5,30 @@
 			<div class="col-xs-4 toggle text-right"><button class="btn btn-default btn-lg" id="btn-toggle"><i class="fa fa-bars"></i></button></div>
 			<div class="col-md-10 col-xs-12 font-sm font-medium text-right">
 				<ul>
-					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/'); ?>">HOME</a></li>
-					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/destinasi'); ?>">DESTINASI</a></li>
-					<li class="hvr-underline-from-left"><a href="#">ABOUT US</a></li>
+					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/'); ?>">Home</a></li>
+					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/destinasi'); ?>">Destinasi</a></li>
+					<li class="hvr-underline-from-left"><a href="#">About Us</a></li>
 					<?php 
 						if($this->session->userdata('hak_akses') == 'user') {
 					?>
-					<li class="hvr-underline-from-left">
-						<a style="text-transform: uppercase;"><?php echo $this->session->userdata('username') ?>, </a>
+					<li class="hvr-underline-from-left" style="overflow: unset;">
+						<a><?php echo $this->session->userdata('username') ?></a>
 					</li>
-					<li class="hvr-underline-from-left">
-					<form action="<?php echo base_url('login/logout') ?>" method="post">
-						<button type="submit" style="border: none;background: transparent;">LOGOUT</button>
-					</form>
+					<li class="hvr-underline-from-left" style="overflow: unset;">
+						<div class="dropdown">
+							<button style="background: transparent; border: none;" type="button" data-toggle="dropdown">
+								<span class="caret"></span></button>
+							</button>
+							<ul class="dropdown-menu" style="min-width: 85px; padding: 8px 12px;">
+								<form action="<?php echo base_url('login/logout') ?>" method="post">
+									<button type="submit" style="border: none;background: transparent;">Logout</button>
+								</form>
+							</ul>
+						</div>
 					</li>
 					<?php } else { ?>
-						<li><a href="<?php echo base_url('/login'); ?>" class="btn bg-purple btn-sm">LOGIN</a></li>
-						<li><a href="#" class="btn btn-default btn-sm">REGISTER</a></li>
+						<li class="log-reg"><a href="<?php echo base_url('/login'); ?>" class="btn bg-purple btn-sm">Login</a></li>
+						<li class="log-reg"><a href="#" class="btn btn-default btn-sm">Register</a></li>
 					<?php } ?>
 				</ul>
 			</div>
