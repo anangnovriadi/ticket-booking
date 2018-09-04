fe04de0b614e6e2843b2d6a21ea5dcf36774c563
anangnov99@gmail.com <anangnov99@gmail.com>
2018-09-04 20:41:22 +0700

    style dropdown

--- application/views/layout/js.php
+++ application/views/layout/js.php
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="<?php echo base_url('assets/front/js/jquery.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/front/js/bootstrap.min.js'); ?>"></script>
+<!-- <script type="text/javascript" src="<?php echo base_url('assets/front/js/bootstrap.min.js'); ?>"></script> -->
 <script type="text/javascript" src="<?php echo base_url('assets/front/js/global.js'); ?>"></script>
-<link rel="stylesheet" type="text/css" href="">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
