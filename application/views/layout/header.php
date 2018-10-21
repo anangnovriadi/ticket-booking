32a60394bdb4073bd417b7cdf76a958c86af8470
anangnov99@gmail.com <anangnov99@gmail.com>
2018-10-21 18:47:41 +0700

    about

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -7,7 +7,7 @@
 				<ul>
 					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/'); ?>">Home</a></li>
 					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/destinasi'); ?>">Destinasi</a></li>
-					<li class="hvr-underline-from-left"><a href="#">About Us</a></li>
+					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/about'); ?>">About Us</a></li>
 					<?php 
 						if($this->session->userdata('hak_akses') == 'user') {
 					?>
@@ -35,25 +35,3 @@
 		</div>
 	</div>
 </div>
-
-<!-- <div class="container-fluid bg-white" id="navbar">
-	<div class="row">
-		<div class="section font-purple-darken">
-			<div class="col-md-2 col-xs-8"><a href="<?php echo base_url('/'); ?>"><img src="<?php echo base_url('assets/front/images/logo-reni.png'); ?>" width="100%"></a></div>
-			<div class="col-xs-4 toggle text-right"><button class="btn btn-default btn-lg" id="btn-toggle"><i class="fa fa-bars"></i></button></div>
-			<div class="col-md-10 col-xs-12 font-sm font-medium text-right">
-				<ul>
-					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/'); ?>">HOME</a></li>
-					<li class="hvr-underline-from-left"><a href="<?php echo base_url('/destinasi'); ?>">DESTINASI</a></li>
-					<li class="hvr-underline-from-left"><a href="#">ABOUT US</a></li>
-					<li class="hvr-underline-from-left">
-						<a style="text-transform: uppercase;">anang</a>
-					</li>
-					<li class="hvr-underline-from-left">
-						<a><img src="<?php echo base_url('assets/front/images/dropdown.png') ?>" /></a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</div> -->
